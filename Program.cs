7c4759c3682c9dfb6ba8546e5bd511b9bc790d11
nitinreddy3 <reddynitin3@gmail.com>
2021-08-21 23:06:29 +0530

    Initialised a program to learn more about c#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyUtilities;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where should we go next May?");
+            WeatherUtilities.Report("Pune", 33, 65 );
+            WeatherUtilities.Report("SF", 65, 73);
+            WeatherUtilities.Report("Delhi", 75, 43);
+        }
+    }
+}
---
+++ WeatherUtilities.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace MyUtilities
+{
+    class WeatherUtilities
+    {
+        static float FahrenheitToCelsius(float temperatureFahrenheit)
+        {
+            return temperatureFahrenheit - 32 / 1.8f;
+        }
+
+        static float CelsiusToFahrenheit(float temperatureCelsius)
+        {
+            return temperatureCelsius * 1.8f + 32;
+        }
+
+        static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        {
+            return (temperatureFahrenheit + humidityPercent) / 4;
+        }
+
+        static public void Report(string location, float temperatureCelsius, float humidity)
+        {
+            var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
+            Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
+        }
+    }
+}
